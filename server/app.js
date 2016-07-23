8cbf8869fe9d0d7fecd46c278246cd99add8df67
Alasdair Munday <alasdair.munday@googlemail.com>
2016-07-23 12:42:57 +0100

    sql file for populating products table, some styling for the phones list

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/bower_components'));
 
 app.listen(port, function() {
   console.log('App listening on port: ' + port);
