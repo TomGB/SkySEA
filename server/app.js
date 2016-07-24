26c9977ef96baf01b6b22d5c82bf705c2b08895a
Joe Williams <joew795@gmail.com>
2016-07-24 15:47:00 +0100

    authentication and model validation on user

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
 var express = require('express');
 var Sequelize = require('sequelize');
-var models = require('./db/models');
 var app = express();
 var models = require('./db/models/index');
 var passport = require('passport');
 var port = process.env.PORT || 3000;
-var sequelize = new Sequelize('mysql://root:@localhost:3000/seanDB');
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(port, function() {
-  console.log('App listening on port: ' + port);
-});
-
 var sequelize = new Sequelize('mysql://root:@localhost/seanDB');
+var bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/public'));
+var bcrypt = require('bcrypt-nodejs');
+app.listen(port, function() {
+    console.log('App listening on port: ' + port);
+});
+app.use(bodyParser({urlencoded: true}));
 
 sequelize
     .authenticate()
     .then(function(err) {
-      console.log('Connection has been established successfully.');
+        console.log('Connection has been established successfully.');
     })
     .catch(function (err) {
-      console.log('Unable to connect to the database:', err);
+        console.log('Unable to connect to the database:', err);
     });
 
 app.get('/api/cases', function (req, res) {
@@ -30,8 +28,54 @@ app.get('/api/cases', function (req, res) {
     });
 });
 
-app.get('/api/users', function(req, res){
-  models.User.findAll({}).then(function(users){
-    res.json(users);
-  })
+app.get('/api/dashboard', function(req, res, next){
+    if(!req.isAuthenticated()){
+        res.redirect('/api/login')
+    }else{
+        var user = req.user;
+        if(user !== undefined){
+            user = user.toJSON();
+        }
+        res.json({user: user});
+    }
+});
+
+app.get('/api/login', function(req, res, next){
+    if(req.isAuthenticated()) res.redirect('/api/dashboard');
+    res.redirect('/api/login');
+});
+
+app.post('/api/login', function(req, res, next){
+    models.User.findAll({where: {email: req.body.email}}).then(function(user){
+        if(user !== null){
+            if(bcrypt.compareSync(req.body.password, user[0].password)){
+                res.redirect('/api/dashboard')
+            }else{
+                res.redirect('/api/login');
+            }
+        }
+    });
+});
+
+app.get('/api/sign-up', function(req, res, next){
+    if(req.isAuthenticated()){
+        res.redirect('/api/dashboard')
+    }else{
+        res.redirect('/api/sign-up');
+    }
+});
+
+app.post('/api/sign-up', function(req, res, next){
+    models.User.create({
+        email: req.body.email,
+        password: models.User.generateHash(req.body.password)
+    }).then(function(user){
+        return res.json(user);
+    }).catch(Sequelize.ValidationError, function(err){
+        return res.status(422).send(err.errors);
+    }).catch(function(err){
+        return res.status(400).send({
+            message: err.message
+        });
+    });
 });
