33ad828246d1001e61e61c1e7221ebe20d4aba5b
Alasdair Munday <alasdair.munday@googlemail.com>
2016-07-25 22:22:36 +0100

    Beginnings of basket functionality on the client. Modularised basket service from the IONIC code.

--- IONIC/www/js/services.js
+++ IONIC/www/js/services.js
@@ -1,123 +1,80 @@
-angular.module('starter.services', [])
+(function(){
+  angular.module('starter.services', ['shopModule'])
+    .factory('uiService', ['$state', '$ionicPopup', 'basketService',
+      function($state, $ionicPopup, basketService){
+        var obj = {};
 
-.factory("basketService", function(){
-  var obj = {};
+        obj.askGoToBasket = function(item){
+          var myPopup = $ionicPopup.show({
+            title: item.name + " added to basket",
+            buttons: [
+              {
+                text: 'Continue Shopping',
+                type: 'button-stable'
+              },
+              {
+                text: 'Go to basket',
+                type: 'button-positive',
+                onTap: function(e){
+                  $state.go('app.basket');
+                }
+              }
+            ]
+          });
+        };
 
-  obj.basketProducts = [];
+        obj.confirmAddToBasket = function(item){
+          var myPopup = $ionicPopup.show({
+            title: 'Add ' + item.name + ' to basket?',
+            buttons: [
+              {
+                text: 'Cancel',
+                type: 'button-assertive'
+              },
+              {
+                text: 'Add',
+                type: 'button-balanced',
+                onTap: function(e){
+                  basketService.addToBasket(item);
+                  obj.askGoToBasket(item);
+                }
+              }
+            ]
+          })
+        };
 
-  obj.addToBasket = function(item){
-    var found = false;
-    var index = 0;
-    obj.basketProducts.forEach(function(elem, ind, arr){
-      if (elem.item.id == item.id){
-        found = true;
-        index = ind;
-      }
-    });
+        return obj;
+      }])
+      .factory('productService', [function(){
+        var obj = {};
 
-    if (!found){
-      // Item is not already in basket
-      obj.basketProducts.push({
-        item: item,
-        quantity: 1
-      });
-    } else {
-      // Item in basket already
-      var productObj = obj.basketProducts[index];
-      productObj.quantity += 1;
-    }
-  };
-
-  obj.removeItem = function(product) {
-    var index = basketService.basketProducts.indexOf(product);
-    obj.basketProducts.splice(index, 1);
-  };
-
-  obj.getTotal = function(){
-    var total = 0;
-    obj.basketProducts.forEach(function(elem, ind, arr){
-      total += elem.item.price * elem.quantity;
-    });
-    return total;
-  };
-  return obj;
-})
-
-.factory('uiService', ['$state', '$ionicPopup', 'basketService',
-  function($state, $ionicPopup, basketService){
-  var obj = {};
-
-  obj.askGoToBasket = function(item){
-    var myPopup = $ionicPopup.show({
-      title: item.name + " added to basket",
-      buttons: [
-        {
-          text: 'Continue Shopping',
-          type: 'button-stable'
-        },
-        {
-          text: 'Go to basket',
-          type: 'button-positive',
-          onTap: function(e){
-            $state.go('app.basket');
+        obj.products = [
+          {
+            id: 1,
+            name: 'iPhone 6 - Game of Thrones',
+            price: 4.99,
+            imgUrl: 'img/phonecases/IPhone6-GOT.jpg'
+          },
+          {
+            id: 2,
+            name: 'HTC 10 - Game of Thrones',
+            price: 4.99,
+            imgUrl: 'img/phonecases/HTC10-GOT.jpg'
+          },
+          {
+            id: 3,
+            name: 'LG G5 - Game of Thrones',
+            price: 4.99,
+            imgUrl: 'img/phonecases/LGG5-GOT.jpg'
+          },
+          {
+            id: 4,
+            name: 'Nexus 6 - Game of Thrones',
+            price: 3.99,
+            imgUrl: 'img/phonecases/Nexus6-GOT.jpg'
           }
-        }
-      ]
-    });
-  };
+        ];
 
-  obj.confirmAddToBasket = function(item){
-    var myPopup = $ionicPopup.show({
-      title: 'Add ' + item.name + ' to basket?',
-      buttons: [
-        {
-          text: 'Cancel',
-          type: 'button-assertive'
-        },
-        {
-          text: 'Add',
-          type: 'button-balanced',
-          onTap: function(e){
-            basketService.addToBasket(item);
-            obj.askGoToBasket(item);
-          }
-        }
-      ]
-    })
-  };
-
-  return obj;
-}])
-
-.factory('productService', [function(){
-  var obj = {};
-
-  obj.products = [
-    {
-      id: 1,
-      name: 'iPhone 6 - Game of Thrones',
-      price: 4.99,
-      imgUrl: 'img/phonecases/IPhone6-GOT.jpg'
-    },
-    {
-      id: 2,
-      name: 'HTC 10 - Game of Thrones',
-      price: 4.99,
-      imgUrl: 'img/phonecases/HTC10-GOT.jpg'
-    },
-    {
-      id: 3,
-      name: 'LG G5 - Game of Thrones',
-      price: 4.99,
-      imgUrl: 'img/phonecases/LGG5-GOT.jpg'
-    },
-    {
-      id: 4,
-      name: 'Nexus 6 - Game of Thrones',
-      price: 3.99,
-      imgUrl: 'img/phonecases/Nexus6-GOT.jpg'
-    }
-  ];
-
-  return obj;
-}]);
+        return obj;
+      }]);
+})();
--- server/client/app/productList/productList.js
+++ server/client/app/productList/productList.js
@@ -2,17 +2,22 @@
  * Created by amu35 on 21/07/2016.
  */
 (function () {
-  angular.module('accessoriesStore')
-      .controller('productListCtrl',['$scope', '$http',function ($scope,$http) {
+  var app = angular.module('accessoriesStore');
+  app.controller('productListCtrl',['$scope', '$http','basketService',function ($scope,$http,basketService) {
 
-          $http.get('/api/cases').then(
-              function (response) {
-                $scope.cases = response.data.cases;
-              },
-              function (response) {
-                  $scope.error = "could not load cases";
-              }
-          );
-  }
-  ]);
+      $scope.basket = basketService.basketProducts;
+      
+      $http.get('/api/cases').then(
+          function (response) {
+            $scope.cases = response.data.cases;
+          },
+          function (response) {
+              $scope.error = "could not load cases";
+          }
+      );
+
+  
+      $scope.addToBasket = basketService.addToBasket;
+      
+  }]);
 })();
