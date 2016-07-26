c31e5cb53c0a1834fe7efbe1044912a544d08c1e
Gurjit Sangha <gurjit.sangha@sky.uk>
2016-07-26 15:48:17 +0100

    commit to merge

--- IONIC/www/js/services.js
+++ IONIC/www/js/services.js
@@ -1,48 +1,5 @@
 angular.module('starter.services', [])
-
-// .factory("basketService", function(){
-//   var obj = {};
-//
-//   obj.basketProducts = [];
-//
-//   obj.addToBasket = function(item){
-//     var found = false;
-//     var index = 0;
-//     obj.basketProducts.forEach(function(elem, ind, arr){
-//       if (elem.item.id == item.id){
-//         found = true;
-//         index = ind;
-//       }
-//     });
-//
-//     if (!found){
-//       // Item is not already in basket
-//       obj.basketProducts.push({
-//         item: item,
-//         quantity: 1
-//       });
-//     } else {
-//       // Item in basket already
-//       var productObj = obj.basketProducts[index];
-//       productObj.quantity += 1;
-//     }
-//   };
-//
-//   obj.removeItem = function(product) {
-//     var index = obj.basketProducts.indexOf(product);
-//     obj.basketProducts.splice(index, 1);
-//   };
-//
-//   obj.getTotal = function(){
-//     var total = 0;
-//     obj.basketProducts.forEach(function(elem, ind, arr){
-//       total += elem.item.price * elem.quantity;
-//     });
-//     return total;
-//   };
-//   return obj;
-// })
-
+  
 .factory('uiService', ['$state', '$ionicPopup', 'basketService',
   function($state, $ionicPopup, basketService){
   var obj = {};
