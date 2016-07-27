767fdcb883285049794ea0e054be06d08391bb7f
Banister <tgbanister@gmail.com>
2016-07-27 10:44:11 +0100

    reimplimented the checkout basket controller

--- server/db/casesPopulate.sql
+++ server/db/casesPopulate.sql
@@ -1,6 +1,17 @@
 
 INSERT INTO productTypes (name) VALUES ('Phone Case');
 
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Frozen HTC10','HTC10-frozen.png',40,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Star Wars HTC10','HTC10-StarWars.png',21,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Sillicon Valley HTC10','HTC10-SiliconV.png',0,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Avengers HTC10','HTC10-Avengers.png',34,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Game of thrones HTC10','HTC10-GOT.png',1,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Star Wars iPhone','IPhone6-StarWars.png',5,4.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Sillicon Valley iPhone','IPhone6-SiliconeV.png',0,4.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Avengers iPhone','IPhone6-Avengers.png',23,4.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Game of thrones iPhone','IPhone6-GOT.png',12,4.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Frozen iPhone','IPhone6-frozen.png',9,4.49);
+
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (1, "PhoneType", "HTC 10");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (2, "PhoneType", "HTC 10");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (3, "PhoneType", "HTC 10");
@@ -22,17 +33,6 @@ INSERT INTO productInfo (productID, infoName, infoValue) VALUES (7, "ShowName", 
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (8, "ShowName", "Avengers");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (9, "ShowName", "Game Of Thrones");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (10, "ShowName", "Frozen");
-
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Frozen HTC10','HTC10-frozen.png',40,5.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Star Wars HTC10','HTC10-StarWars.png',21,5.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Sillicon Valley HTC10','HTC10-SiliconV.png',0,5.49);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Avengers HTC10','HTC10-Avengers.png',34,5.49);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Game of thrones HTC10','HTC10-GOT.png',1,5.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Star Wars iPhone','IPhone6-StarWars.png',5,4.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Sillicon Valley iPhone','IPhone6-SiliconeV.png',0,4.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Avengers iPhone','IPhone6-Avengers.png',23,4.99);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Game of thrones iPhone','IPhone6-GOT.png',12,4.49);
-Insert INTO products (name,imageUrl,availableStock,price) Values ('Frozen iPhone','IPhone6-frozen.png',9,4.49);
 
 Insert INTO workers (firstname, lastname, email, password, active) Values ('tom','banister','tgbanister@gmail.com','password',false);
 Insert INTO workers (firstname, lastname, email, password, active) Values ('tim','miles','tmiles@gmail.com','password',false);
