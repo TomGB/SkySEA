a562d0f38bde32ce9135f65b0f493a267b0b2bb1
Alasdair Munday <alasdair.munday@googlemail.com>
2016-07-28 09:59:49 +0100

    ssl added but not in use

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ var app = express();
 var passport = require('passport');
 var port = process.env.PORT || 3000;
 var sequelize = new Sequelize('mysql://root:@localhost/seanDB');
+var https = require('https');
+var fs = require('fs');
+
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,4 +15,16 @@ app.listen(port, function() {
   console.log('App listening on port: ' + port);
 });
 
+var options = {
+  key: fs.readFileSync('./ssl/69546497-localhost_3000.key'),
+  cert: fs.readFileSync('./ssl/69546497-localhost_3000.cert'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
+// var server = https.createServer(options, app).listen(port, function(){
+//   console.log("server started at port 3000");
+// });
+
 app.use('/api',require('./libs/APIrouter'));
+
