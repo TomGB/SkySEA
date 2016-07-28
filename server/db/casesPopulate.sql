9e78f9ed4ed9e10253e1f1e5f7267888ca9bcbc6
Banister <tgbanister@gmail.com>
2016-07-28 11:19:03 +0100

    Added 13 new phone cases

--- server/db/casesPopulate.sql
+++ server/db/casesPopulate.sql
@@ -12,6 +12,21 @@ Insert INTO products (name,imageUrl,availableStock,price) Values ('Avengers iPho
 Insert INTO products (name,imageUrl,availableStock,price) Values ('Game of thrones iPhone','IPhone6-GOT.png',12,4.49);
 Insert INTO products (name,imageUrl,availableStock,price) Values ('Frozen iPhone','IPhone6-frozen.png',9,4.49);
 
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Tie Fighter iPhone 6','IPhone6-TFighter.png',40,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Beach Fight iPhone 6','IPhone6-StarWarsBeach.png',21,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Rey iPhone 6','IPhone6-Rey.png',14,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('PoeiPhone 6','IPhone6-Poe.png',34,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Millennium Falcon iPhone 6','IPhone6-MF.png',24,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Jakku iPhone 6','IPhone6-Jakku.png',34,4.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('BB8 iPhone 6','IPhone6-BB8.png',53,4.99);
+
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Beach Fight HTC 10','HTC10-StarWarsSand.png',40,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Rey HTC 10','HTC10-Rey.png',21,5.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Poe HTC 10','HTC10-Poe.png',14,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Millennium Falcon HTC 10','HTC10-MF.png',34,5.49);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('Jakku HTC 10','HTC10-Jakku.png',34,4.99);
+Insert INTO products (name,imageUrl,availableStock,price) Values ('BB8 HTC 10','HTC10-BB8.png',53,4.99);
+
 Insert INTO workers (firstname, lastname, email, password, active) Values ('tom','banister','tgbanister@gmail.com','password',false);
 Insert INTO workers (firstname, lastname, email, password, active) Values ('tim','miles','tmiles@gmail.com','password',false);
 
@@ -26,6 +41,23 @@ INSERT INTO productInfo (productID, infoName, infoValue) VALUES (8, "PhoneType",
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (9, "PhoneType", "iPhone 6");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (10, "PhoneType", "iPhone 6");
 
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (11, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (12, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (13, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (14, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (15, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (16, "PhoneType", "iPhone 6");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (17, "PhoneType", "iPhone 6");
+
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (18, "PhoneType", "HTC 10");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (19, "PhoneType", "HTC 10");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (20, "PhoneType", "HTC 10");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (21, "PhoneType", "HTC 10");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (22, "PhoneType", "HTC 10");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (23, "PhoneType", "HTC 10");
+
+
+
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (1, "ShowName", "Frozen");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (2, "ShowName", "Star Wars");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (3, "ShowName", "Silicon Valley");
@@ -36,6 +68,20 @@ INSERT INTO productInfo (productID, infoName, infoValue) VALUES (7, "ShowName", 
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (8, "ShowName", "Avengers");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (9, "ShowName", "Game Of Thrones");
 INSERT INTO productInfo (productID, infoName, infoValue) VALUES (10, "ShowName", "Frozen");
+
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (11, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (12, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (13, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (14, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (15, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (16, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (17, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (18, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (19, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (20, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (21, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (22, "ShowName", "Star Wars");
+INSERT INTO productInfo (productID, infoName, infoValue) VALUES (23, "ShowName", "Star Wars");
 
 
 INSERT INTO `users` (`id`, `email`, `password`, `address1`, `address2`, `address3`, `postcode`, `firstName`, `lastName`, `updatedAt`, `createdAt`, `userRole`)
